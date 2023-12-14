22830111da2faf95d54e80522932961486f599c3
abed7helmi <abed.helmi@yahoo.fr>
2023-12-14 21:28:14 +0100

    override metode convert

--- src/main/java/org/example/service/BankGrpcService.java
+++ src/main/java/org/example/service/BankGrpcService.java
@@ -7,7 +7,22 @@ import org.example.stubs.Ebank;
 public class BankGrpcService extends BankServiceGrpc.BankServiceImplBase {
     @java.lang.Override
     public void convert(Ebank.ConvertCurrencyRequest request, StreamObserver<Ebank.ConvertCurrencyResponse> responseObserver) {
-        super.convert(request, responseObserver);
+        String currencyForm = request.getCurrencyFrom();
+        String currencyTo = request.getCurrencyTo();
+
+        double amount = request.getAmount();
+
+        Ebank.ConvertCurrencyResponse response= Ebank.ConvertCurrencyResponse.newBuilder()
+                .setCurrencyFrom(currencyForm)
+                .setCurrencyTo(currencyTo)
+                .setAmount(amount)
+                .setResult(amount*11.4)
+                .build();
+
+        // envoie rep
+        responseObserver.onNext(response);
+        // indiquer qu'on a fini
+        responseObserver.onCompleted();
     }
 
     @java.lang.Override
