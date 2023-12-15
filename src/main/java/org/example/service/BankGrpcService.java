24c1982841a4690b1e7da6e7b20b0a27f85a7dc9
abed7helmi <abed.helmi@yahoo.fr>
2023-12-15 13:05:26 +0100

    streaming server

--- src/main/java/org/example/service/BankGrpcService.java
+++ src/main/java/org/example/service/BankGrpcService.java
@@ -4,9 +4,13 @@ import io.grpc.stub.StreamObserver;
 import org.example.stubs.BankServiceGrpc;
 import org.example.stubs.Ebank;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class BankGrpcService extends BankServiceGrpc.BankServiceImplBase {
     @java.lang.Override
     public void convert(Ebank.ConvertCurrencyRequest request, StreamObserver<Ebank.ConvertCurrencyResponse> responseObserver) {
+        System.out.println("test unray");
         String currencyForm = request.getCurrencyFrom();
         String currencyTo = request.getCurrencyTo();
 
@@ -25,9 +29,29 @@ public class BankGrpcService extends BankServiceGrpc.BankServiceImplBase {
         responseObserver.onCompleted();
     }
 
+
+    // Server streaming model
     @java.lang.Override
     public void getStream(Ebank.ConvertCurrencyRequest request, StreamObserver<Ebank.ConvertCurrencyResponse> responseObserver) {
-        super.getStream(request, responseObserver);
+        System.out.println("test server streaming");
+        double amount=request.getAmount();
+        Timer timer=new Timer();
+       // chaque seconde j'envoie
+        timer.schedule(new TimerTask() {
+            int counter=0;
+            @Override
+            public void run() {
+                Ebank.ConvertCurrencyResponse response= Ebank.ConvertCurrencyResponse.newBuilder()
+                        .setResult(amount*Math.random()*11)
+                        .build();
+                responseObserver.onNext(response);
+                ++counter;
+                if(counter==10){
+                    responseObserver.onCompleted();
+                    timer.cancel();
+                }
+            }
+        }, 1000, 1000);
     }
 
     @java.lang.Override
